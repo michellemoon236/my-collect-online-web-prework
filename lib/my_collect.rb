7310f5e7a7bda77f6e35cf0f903891ec17b7e210
Michelle M <michellemoon236@gmail.com>
2019-03-22 20:12:48 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,9 @@
+  languages = ['ruby', 'javascript', 'python', 'objective-c']
+  students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller'] }
+
 def my_collect (array)
   i = 0 
-  if array.include?(" ")
+  if array.include?(students)
     while i < array.length 
       yield array.split(" ").first
       i = i + 1 
@@ -12,7 +15,6 @@ def my_collect (array)
     end
 
 end
-array_new
 
 end 
   
