962d04f2858fe59c22e3be89d11a700f8c06d234
Michelle M <michellemoon236@gmail.com>
2019-03-22 21:07:42 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
+def my_collect (array)
+   i = 0 
+   array_new = []
+  # return array if array.empty?
+  # if array[0].include?(" ")
+  #   while i < array.length 
+  #     array_new<< (array[i]).split(" ").first
+  #     i = i + 1 
+  #   end
+  # else
+    while i < array.length 
+      array_new<< yield(array[i])#.upcase
+      i = i + 1  
+    end
+#end
+array_new
+end 
+
 
 
