a8629197cf0209c35d8b8f89d8721e8fee1c623e
Michelle M <michellemoon236@gmail.com>
2019-03-22 19:59:01 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+def my_collect (array)
+   i = 0 
+  if array.include?(" ")
+    while i < array.length 
+      yield array.split(" ").first
+      i = i + 1 
+    end
+  else
+    while i < array.length 
+      yield array.upcase
+      i = i + 1  
+    end
 
+end
+  
+  
 
