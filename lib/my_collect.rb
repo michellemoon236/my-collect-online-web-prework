965ce829a3bb02bc46d338d70415e13b11f2ab99
Michelle M <michellemoon236@gmail.com>
2019-03-22 20:08:32 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,14 +1,13 @@
 def my_collect (array)
   i = 0 
-  array_new = []
   if array.include?(" ")
     while i < array.length 
-      array_new << yield array.split(" ").first
+      yield array.split(" ").first
       i = i + 1 
     end
   else
     while i < array.length 
-      array_new << yield array.upcase
+      yield array.upcase
       i = i + 1  
     end
 
