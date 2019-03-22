f956655fb51c6bf8c0e4ee81fc8c10e499a88b6f
Michelle M <michellemoon236@gmail.com>
2019-03-22 20:15:28 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,12 +4,12 @@
 def my_collect (array)
   i = 0 
   if array.include?(students)
-    while i < array.length 
+    while i<array.length 
       yield array.split(" ").first
       i = i + 1 
     end
   else
-    while i < array.length 
+    while i<array.length 
       yield array.upcase
       i = i + 1  
     end
