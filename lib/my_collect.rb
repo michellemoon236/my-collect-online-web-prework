a64a825a3a5c48c6fea933aa5bd21c57e64fb4ba
Michelle M <michellemoon236@gmail.com>
2019-03-22 20:03:05 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,11 @@
 def my_collect (array)
-
+  i = 0 
   if array.include?(" ")
-       i = 0 
     while i < array.length 
       yield array.split(" ").first
       i = i + 1 
     end
   else
-       i = 0 
     while i < array.length 
       yield array.upcase
       i = i + 1  
