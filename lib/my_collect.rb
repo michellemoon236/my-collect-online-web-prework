486a2c776c5b6a3248955d31f4714625d7659c1e
Michelle M <michellemoon236@gmail.com>
2019-03-22 20:20:59 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,16 @@
-  languages = ['ruby', 'javascript', 'python', 'objective-c']
-  students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
-
 def my_collect (array)
-i = 0
+   i = 0 
+  if array.include?(" ")
     while i < array.length 
       yield array[i].split(" ").first
       i = i + 1 
     end
-
+  else
+    while i < array.length 
+      yield array[i].upcase
+      i = i + 1  
+    end
 end
+
   
 
