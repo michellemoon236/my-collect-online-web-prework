9744ad442ec1e54ec7733e31b52ef3e4bfde97a5
Michelle M <michellemoon236@gmail.com>
2019-03-22 20:02:36 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -7,8 +7,8 @@ def my_collect (array)
       i = i + 1 
     end
   else
-    while i < array.length 
        i = 0 
+    while i < array.length 
       yield array.upcase
       i = i + 1  
     end
